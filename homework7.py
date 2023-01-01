b24b3db559916d7a945a30c45f641e18f03a75e2
Nurbek <unurbek499@gmail.com>
2023-01-01 17:43:30 +0600

    ready

--- homework7.py
+++ homework7.py
@@ -1,7 +1,19 @@
 def get_list() -> list:
     return list(range(0, 1_000_000, 2))
 
+search_for = 4  # что ищем
 
+
+def linear_search(where, what):
+    for k in enumerate(where):
+        if k[1] == what:
+            return k[0]  # возвращаем индекс
+
+    return None
+
+
+print('искомый элемент', search_for, 'найден под индексом',
+      linear_search(get_list(), search_for))
 """
     what is Binary Search?
     then do Solution for search target in list
@@ -16,18 +28,3 @@ class Solution:
     def find_target(self, list, target):
         pass
 
-name = [1,2,3,4,5,6,7,8,9]
-
-search_for = 4  # что ищем
-
-
-def linear_search(where, what):
-    for k in enumerate(where):
-        if k[1] == what:
-            return k[0]  # возвращаем индекс
-
-    return None
-
-
-print('искомый элемент', search_for, 'найден под индексом',
-      linear_search(name, search_for))
